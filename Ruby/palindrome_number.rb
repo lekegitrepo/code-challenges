1df0e39dec94bdd25ea75ca45e7e74310dfabb32
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-06-28 09:02:45 +0100

    Add more solutions

--- Ruby/palindrome_number.rb
+++ Ruby/palindrome_number.rb
@@ -4,6 +4,20 @@ def is_palindrome(x)
   x.to_s.reverse.to_i == x
 end
 
+# def is_palindrome(x, y = x.to_s)
+#   y == y.reverse
+# end
+
+# def is_palindrome(x)
+#   n = x
+#   reverse_x = 0
+#   while(n > 0)
+#     reverse_x = (reverse_x*10 + (n%10))
+#     n /= 10
+#   end
+#   x == reverse_x
+# end
+
 p is_palindrome(121)
 
 p is_palindrome(-121)
